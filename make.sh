dc733f8b3ff75cd1b32b60d67e19cb290dd06ada
duoli <zhaody901@126.com>
2021-08-11 19:32:59 +0800

    auto docs

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 make html
 wkd=$(cd "$(dirname "$0")" && pwd)
 mkdir "$wkd/docs"
-cp -r "$wkd/_build/html/" "$wkd/_build/docs/"
+cp -r "$wkd/_build/html/*" "$wkd/_build/docs/"
 git add --all
 git commit -m 'auto docs'
 git push
